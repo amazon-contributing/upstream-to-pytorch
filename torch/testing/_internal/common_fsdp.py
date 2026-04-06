4f7b46003dd5e9308f9281e931f9a098f89abaad
Abigail Fernandes <ferabiga@amazon.com>
2026-04-06 23:10:25 +0000

    fix: cleanup from rebase

--- torch/testing/_internal/common_fsdp.py
+++ torch/testing/_internal/common_fsdp.py
@@ -1188,7 +1188,7 @@ def check_sharded_parity(
         cls.assertEqual(sharded_param.grad.to_local(), sharded_ref_grad.to_local())
 
 
-@unittest.skipIf(TEST_XPU or TEST_PRIVATEUSE1, "not-support-multithread")
+@unittest.skipIf(TEST_XPU, "not-support-multithread")
 class FSDPTestMultiThread(MultiThreadedTestCase):
     @property
     def world_size(self):
