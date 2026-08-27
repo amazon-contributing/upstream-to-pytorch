de4951b9319028a4f124787417f15f1649f0d5c8
Phillip Taylor <25676208+philliptay@users.noreply.github.com>
2026-08-27 14:51:20 -0700

    Make PP tests hardware-agnostic (#107)

--- test/distributed/pipelining/test_unflatten.py
+++ test/distributed/pipelining/test_unflatten.py
@@ -74,10 +74,7 @@ class UnflattenTests(TestCase):
         print(f"Equivalence test passed {torch.sum(out)} ref {torch.sum(ref)}")
 
 
-devices = ["cpu", "cuda", "hpu", "xpu"]
-instantiate_device_type_tests(
-    UnflattenTests, globals(), only_for=devices, allow_xpu=True
-)
+instantiate_device_type_tests(UnflattenTests, globals(), allow_xpu=True)
 
 if __name__ == "__main__":
     run_tests()
